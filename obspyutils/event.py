c75e19a470f1749ddb2f147039c9eb90ab355ebf
Brad Aagaard <baagaard@usgs.gov>
2015-03-10 17:13:46 -0700

    Add search for focal mechanism and extraction of DC moment tensor.

--- obspyutils/event.py
+++ obspyutils/event.py
@@ -21,6 +21,15 @@ def find_origin(event, methodId):
     return
 
 # ----------------------------------------------------------------------
+def find_focalmechanism(event, methodId):
+    for fm in event.focal_mechanisms:
+        if methodId == fm.method_id.id:
+            return fm
+    raise ValueError("Could not find focal mechanism in event %s for method %s." % \
+                     (event, methodId))
+    return
+
+# ----------------------------------------------------------------------
 def moment_tensor(event):
     for fm in event.focal_mechanisms:
         if not fm.moment_tensor.resource_id is None:
@@ -34,6 +43,17 @@ def first_motion(event):
         if 'nodal_plane_1' in fm.nodal_planes:
             return fm
     raise ValueError("Could not find focal mechanism with nodal planes in event %s." % event)
+    return
+
+
+# ----------------------------------------------------------------------
+def add_dconly(event):
+    for fm in event.focal_mechanisms:
+        if not fm.moment_tensor.resource_id is None:
+            import obspyutils.momenttensor
+            mtdc = obspyutils.momenttensor.extractDC(fm.moment_tensor)
+            event.focal_mechanisms.append(mtdc)
+            return
     return
 
 
