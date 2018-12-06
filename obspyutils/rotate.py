7b6b69982bfdc720e112bbfd290251cf46e540d1
Brad Aagaard <baagaard@usgs.gov>
2018-12-06 12:36:32 -0800

    Better fix for enforcing same start time and end time across components.

--- obspyutils/rotate.py
+++ obspyutils/rotate.py
@@ -32,21 +32,25 @@ def toENZ(inventory, stream):
         ncode, scode = tuple(stkey.split('.'))
         station = inventory.select(network=ncode, station=scode).networks[0].stations[0]
 
+        # Find min starttime and max end time
         tr0 = streamO.traces[0]
-        shape = tr0.data.shape
-        dataE = numpy.zeros(shape)
-        dataN = numpy.zeros(shape)
-        dataZ = numpy.zeros(shape)
-
-        starttime = tr0.stats.starttime
-        endtime = tr0.stats.endtime 
+        start_min = tr0.stats.starttime
+        end_max = tr0.stats.endtime
+        for tr in streamO.traces[1:]:
+            start_min = min(start_min, tr.stats.starttime)
+            end_max = max(end_max, tr.stats.endtime)
+        npts = int(1 + (end_max - start_min)*tr0.stats.sampling_rate)
+        
+        dataE = numpy.zeros(npts)
+        dataN = numpy.zeros(npts)
+        dataZ = numpy.zeros(npts)
 
         fail = False
         for tr in streamO.traces:
             cinventory = inventory.select(network=tr.stats.network, station=tr.stats.station, channel=tr.stats.channel)
             channel = cinventory.networks[0].stations[0].channels[0]
-            trC = tr.copy().trim(starttime=starttime, endtime=endtime, pad=True, fill_value=0)
-            if trC.data.shape != shape:
+            trC = tr.copy().trim(starttime=start_min, endtime=end_max, pad=True, fill_value=0)
+            if trC.data.shape[0] != npts:
                 print "Mismatch in shape for channels of station: %s" % stkey
                 fail = True
                 break
